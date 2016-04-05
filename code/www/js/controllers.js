116ccc64017c8e1e633dada056cf43462fbe4300
bhushan <bgavhale@PUNHWD14557.igatecorp.com>
2016-04-05 19:54:21 +0530

    changes for 04/05 - 1

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,12 +4,33 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+	// helper function for loading
+	var showLoading = function() {
+		$ionicLoading.show({
+			template: '<i class="ion-loading-c"></i>',
+			noBackdrop: true
+		});
+	}
+
+	var hideLoading = function() {
+		$ionicLoading.hide();
+	}
+
+	// set loading to true first time while we retrive songs from server
+	showLoading();
+
 	//get our first songs
-	Recommendations.getNextSongs()
+	Recommendations.init()
 		.then(function() {
 			$scope.currentSong = Recommendations.queue[0];
+			Recommendations.playCurrentSong();		
 		})
+		.then(function() {
+			// turn loading off
+			hideLoading();
+			$scope.currentSong.loaded = true;
+		});
 
 	$scope.songs = [];
 
@@ -30,7 +51,12 @@ Controller for the discover page
 		$timeout(function (){
 			//timeout to allow animation to complete
 			$scope.currentSong = Recommendations.queue[0];
+			$scope.currentSong.loaded = true;
 		}, 250);
+
+		Recommendations.playCurrentSong().then(function() {
+			$scope.currentSong.loaded = false;
+		});
 		
 	}
 
@@ -50,12 +76,16 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
+.controller('FavoritesCtrl', function($scope, $window, User) {
 	// get the list of our favorites from the User service
 	$scope.favorites = User.favorites;
 
 	$scope.removeSong = function(song, index) {
 		User.removeSongFromFavorites(song, index);
+	}
+
+	$scope.openSong = function(song) {
+		$window.open(song.open_url, "_system");
 	}
 
 })
@@ -64,6 +94,33 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, User, Recommendations) {
+	// expose the number of new favorites to the scope
+	$scope.favCount = User.favoriteCount;
 
+	// stop audio when going to favorites tab
+	$scope.enteringFavorites = function() {
+		User.newFavorites = 0;
+		Recommendations.haltAudio();
+	}
+
+	$scope.leavingFavorites = function() {
+		Recommendations.init();
+	}
+
+})
+
+.controller('SplashCtrl', function($scope, $state, User){
+
+	// attempt to signup/login via User.auth
+	$scope.submitForn = function(username , signingUp) {
+		User.auth(username, signingUp).then(function() {
+			// session is now set, so lets redirect to discover page
+			$state.go('tab.discover');
+		}, function() {
+			// error handling here
+			alert('Hmm...try another username');
+		});
+	}
+	
 });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,8 +1,25 @@
 angular.module('songhop.services', [])
-	.factory('User', function(){
+	.factory('User', function($http, SERVER){
 	
 		var o = {
-			favorites: []
+			username: false,
+			session_id: false,
+			favorites: [],
+			newFavorites: 0
+		}
+
+		// attempt login or signup
+		o.auth = function(username, signingUp) {
+
+			var authRoute;
+
+			if (signingUp) {
+				authRoute = 'signup';				
+			} else {
+				authRoute = 'login';
+			}
+
+			return $http.post(SERVER.url + '/' + authRoute, {username: username});
 		}
 
 		o.addSongToFavorites = function(song){
@@ -11,6 +28,10 @@ angular.module('songhop.services', [])
 
 			//add to favorites array
 			o.favorites.unshift(song);
+			o.newFavorites++;
+
+			// persist this to the server
+			return $http.post(SERVER.url + '/favorites', {session_id: o.session_id, song_id: song.song_id});
 		}
 		  
 		o.removeSongFromFavorites = function(song, index){
@@ -19,15 +40,72 @@ angular.module('songhop.services', [])
 
 			//remove from favorites array
 			o.favorites.splice(index, 1);
+
+			// persist this to the server
+			return $http({
+				method: 'DELETE',
+				url: SERVER.url + '/favorites',
+				params: { session_id: o.session_id, song_id: song.song_id}
+			});
+		}
+
+	 // gets the entire list of this user's favs from server
+	  o.populateFavorites = function() {
+	    return $http({
+	      method: 'GET',
+	      url: SERVER.url + '/favorites',
+	      params: { session_id: o.session_id }
+	    }).success(function(data){
+	      // merge data into the queue
+	      o.favorites = data;
+	    });
+	  }		
+
+		o.favoriteCount = function() {
+			return o.newFavorites;
 		}
 
 		return o;
 	})
 
-	.factory('Recommendations', function($http, SERVER){
+	.factory('Recommendations', function($http, SERVER, $q){
 		var o = {
 			queue: []
 		};
+
+		var media;
+
+		o.init = function() {
+			if (o.queue.length === 0) {
+				// if there is nothing in the queue, fill it.
+				// this also means that this is the first call of init
+				return o.getNextSongs();
+			} else {
+				// otherwise, play the current song
+				return o.playCurrentSong();
+			}
+		}
+
+		o.playCurrentSong = function() {
+			var defer = $q.defer();
+
+			//play the current song's preview
+			media = new Audio(o.queue[0].preview_url);
+
+			//when song loaded, resolve the promise to let controller know
+			media.addEventListener("loadeddata", function() {
+				defer.resolve();
+			});
+
+			media.play();
+
+			return defer.promise;
+		}
+
+		//used when switching to favorites tab
+		o.haltAudio = function() {
+			if (media) media.pause();
+		}
 
 		o.getNextSongs = function() {
 			return $http({
@@ -42,6 +120,9 @@ angular.module('songhop.services', [])
 		o.nextSong = function() {
 			// pop the index 0 off
 			o.queue.shift();
+
+			// end the song
+			o.haltAudio();
 
 			//low on queue? lets fill it up
 			if (o.queue.length <= 3){
